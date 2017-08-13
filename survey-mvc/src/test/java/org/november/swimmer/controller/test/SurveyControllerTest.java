fde094265228d9e9e7f8b1ab6d26f49ba8079441
novemberswimmer <ruelsantiago@gmail.com>
2017-08-12 21:20:34 -0500

    added home.html template.
    added home.html template.

--- survey-mvc/src/test/java/org/november/swimmer/controller/test/SurveyControllerTest.java
+++ survey-mvc/src/test/java/org/november/swimmer/controller/test/SurveyControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.november.swimmer.controller.SurveyController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
